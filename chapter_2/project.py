549ad77785e0c5f90687f7eaa6fc0ba9d117d87f
aihorizons <dave@theaihorizons.com>
2024-10-24 14:52:24 -0700

    chapter 2 project files

---
+++ chapter_2/project.py
@@ -0,0 +1,10 @@
+# Simple Input and Output Project
+
+# Ask the user for their favorite color
+favorite_color = input("What is your favorite color? ")
+
+# Ask the user for their favorite food
+favorite_food = input("What is your favorite food? ")
+
+# Print a message that includes both inputs
+print(f"Your favorite color is {favorite_color} and your favorite food is {favorite_food}!")
